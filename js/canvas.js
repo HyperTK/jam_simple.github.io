994892985517526fa6c3ed19852994b866ec8eb3
tk_thunder <kakeru.t0227@gmail.com>
2019-07-24 13:54:51 +0900

    commit

--- js/canvas.js
+++ js/canvas.js
@@ -82,6 +82,12 @@ function imgDownload() {
             var w = pointer[i].clientWidth;
             var x = pointer[i].offsetLeft;
             var y = pointer[i].offsetTop;
+
+            img.drawOffsetX = x;
+            img.drawOffsetY = y;
+            img.drawWidth = 50;
+            img.drawHeigth = 50;
+
             var rect = $("#target").offset();
             context.drawImage(img, x, y, 50, 50);
         }
