dd4d5a498a63d1b309527af5d3d41f2bff9dbe79
tk_thunder <kakeru.t0227@gmail.com>
2019-07-24 16:09:02 +0900

    commit

--- js/canvas.js
+++ js/canvas.js
@@ -84,7 +84,7 @@ function imgDownload() {
             var y = pointer[i].offsetTop;
 
             var rect = $("#target").offset();
-            ctx.drawImage(img, 0, 0, x, y, 0, 0, 100, 100);
+            ctx.drawImage(img, x, y, 50, 50, 0, 0, 50, 50);
         }
         var c = $("#target").get(0);
         // 画像として出力
