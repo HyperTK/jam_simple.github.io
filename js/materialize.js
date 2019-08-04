28acaa9078ed6fad6538c6c5b23dc8bbe02946bf
tk_thunder <kakeru.t0227@gmail.com>
2019-08-05 08:41:07 +0900

    materialize.jsにmodal追記

--- js/materialize.js
+++ js/materialize.js
@@ -2,4 +2,5 @@ $(document).ready(function () {
     $('.collapsible').collapsible();
     $('select').formSelect();
     $('input#contributor, input#problem').characterCounter();
+    $('.modal').modal();
 });
