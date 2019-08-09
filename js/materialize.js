380623b8b6451eb7a355339a279304c840c5461a
tk_thunder <kakeru.t0227@gmail.com>
2019-08-09 16:29:27 +0900

    materialize.js ドロップダウン追加

--- js/materialize.js
+++ js/materialize.js
@@ -3,4 +3,5 @@ $(document).ready(function () {
     $('select').formSelect();
     $('input#contributor, input#problem').characterCounter();
     $('.modal').modal();
+    $(".dropdown-trigger").dropdown();
 });
